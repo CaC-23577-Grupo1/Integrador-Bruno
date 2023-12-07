a9c282eb649fd0d0c0780b4b3e3debf33e870a18
Bruno Belforte <brunobelforte@gmail.com>
2023-12-07 13:50:03 -0300

    Bru503: Se corrigio la forma de enviar la data desde el JSON hacia la vista EJS.  Solo se implemento en ITEMS, aunque falta mejorar el mensaje de error al solicitar Item inexistente, es decir darle estilo.

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -3,8 +3,9 @@
 
 const path = require('path');
 
-const { traerTodosLosProductos } = require('../models/itemsModel');
+const { traerUnSoloProducto } = require('../models/itemsModel');
 const { param } = require('../routes/mainRoutes');
+const { log } = require('console');
 
 
 /* creamos un objeto que contendra diferentes propiedades y cada una de ellas sera una "funcion" para dar logica de respuesta a cada ruta */
@@ -17,24 +18,16 @@ const shopControllers = {
   }),
 
 
-
-  // Definimos la "funcion" que cumplira el controlador de la ruta items en funcion asincrona ya que debemos aguardar recibir datos desde el JSON mediante el Modelo
-  item: async (req, res) => {
-
-    const { id } = req.params;  // Guardamos en una variable el ID recibido por la ruta
-    const dataRecibida = await traerTodosLosProductos();  // Recibimos un array  con la data recibida desde el Modelo proveniente del JSON y lo guardamos en "dataRecibida"
-    const posicionArray = dataRecibida.findIndex(productosRecibidos => productosRecibidos.prod_id == id); // Identificamos el Inidice del array que contiene el Producto que buscamos segun la ID
-    const dataParaVista = [dataRecibida[posicionArray]];  // Aislamos la data de ese solo elemento del array que nos interesa, ademas lo encerramos en corchetes para que sea un array y poder enviarlo a la vista como array
-
-    //  NOTA:  Ahora falta el manejo de errores para cuando un ID no existe, mensaje de error y todo ello                <--------------------------------------------------------
-
-    //  NOTA EXTRA:   este metodo si bien es funcional, luego de ver las clases 26 y 27 de la comision 23567 he descubierto que no es el mas practico, por lo cual procedere ahora a modificar el codigo
-    //                para obtener en la capa modelos diferentes "funciones" que podremos llamar y nos devolveran la data completa o filtrada dependiendo a que "funcion" llamemos.
+  
+   // Definimos la "funcion" que cumplira el controlador de la ruta items como asincrona ya que debemos aguardar recibir datos desde el JSON mediante el Modelo
+   item: async (req, res) => {
+     
+    const dataRecibida = await traerUnSoloProducto(req.params.id);    // requerimos la funcion definida en el modelo pasandole como argumento el ID del Path Params que nos entrega la ruta
 
     res.render('./shop/item',
       {
         title: "Item | Funkoshop",
-        dataParaVista
+        dataRecibida
       })
   },
 
--- src/models/itemsModel.js
+++ src/models/itemsModel.js
@@ -1,17 +1,39 @@
+// Este archivo pertenece a la capa de modelos y es el encargado de....
+// ¿interactuar con la base de datos para traer la infoamacion y entregarsela al controlador segun sus requerimentos?
+
 const fs = require('fs');
 const path = require('path');
 
 
+// Creamos una "funcion" que traera TODOS producto del JSON en base a un ID de producto recibido
 const traerTodosLosProductos = async () => {
 
-    const dataDelJson =  await fs.readFileSync(path.resolve(__dirname, '../../public/data/products.json'));
-    const datosJsonParseados =  await JSON.parse(dataDelJson);
+    const dataDelJson =  await fs.readFileSync(path.resolve(__dirname, '../../public/data/products.json'));     // Inicialmente leemos el JSON
+    const datosJsonParseados =  await JSON.parse(dataDelJson);                                                  // Parseamos la Dara recibida para que su estructura/formato nos sea util
 
-    return datosJsonParseados;
+    return datosJsonParseados;                                                                                  // Retornamos la data completa a quien "requirio" esta funcion
 
 };
 
 
+
+// Creamos una "funcion" que traera SOLO UN producto del JSON en base a un ID de producto recibido
+const traerUnSoloProducto = async (idBuscado) => {
+
+    const dataDelJson =  await fs.readFileSync(path.resolve(__dirname, '../../public/data/products.json'));     // Inicialmente leemos el JSON
+    const datosJsonParseados =  await JSON.parse(dataDelJson);                                                  // Parseamos la Dara recibida para que su estructura/formato nos sea util
+
+    const productoSeleccionado = datosJsonParseados.filter(producto => producto.prod_id == idBuscado);          // Dentro de la data recibida filtraremos 1 solo producto con el ID recibido
+                                                                                                                // En caso de no existir coincidencias obtendremos un Array vacio
+
+    return productoSeleccionado;                                                                                // Retornamos la data a quien "requirio" esta funcion
+
+};
+
+
+
+// Exportamos estas "funciones" para que esten disponibles por quien requiera este modulo de la capa modelos
 module.exports = {
-    traerTodosLosProductos
+    traerTodosLosProductos,
+    traerUnSoloProducto
 };
